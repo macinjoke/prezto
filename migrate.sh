23b7f15ea82def2bdea7761f1bca6622fa2919e4
Shunji Makino <shunji.makino@gmail.com>
2018-05-07 01:26:19 +0900

    add migrate.sh

---
+++ migrate.sh
@@ -0,0 +1,9 @@
+ln -s .ghq/github.com/macinjoke/prezto ~/.zprezto
+ln -s .ghq/github.com/macinjoke/prezto/runcoms/zlogin ~/.zlogin
+ln -s .ghq/github.com/macinjoke/prezto/runcoms/zlogout ~/.zlogout
+ln -s .ghq/github.com/macinjoke/prezto/runcoms/zpreztorc ~/.zpreztorc
+ln -s .ghq/github.com/macinjoke/prezto/runcoms/zprofile ~/.zprofile
+ln -s .ghq/github.com/macinjoke/prezto/runcoms/zshenv ~/.zshenv
+ln -s .ghq/github.com/macinjoke/prezto/runcoms/zshrc ~/.zshrc
+# run depending on your pc environment
+# ln -s .ghq/github.com/macinjoke/prezto/runcoms/local/[hoge] ~/.zshenv
